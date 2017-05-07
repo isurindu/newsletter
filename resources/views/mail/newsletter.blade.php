2ac54d455d6d4f9141668f080ee949b9c2863652
isurindu <isurindu.prabashwara@gmail.com>
2017-05-07 22:16:56 +0400

    newsletter layout done

--- resources/views/mail/newsletter.blade.php
+++ resources/views/mail/newsletter.blade.php
@@ -47,7 +47,56 @@ body, table, td {font-family: 'capsuularegular' !important;}
           <td  style="font-family:capsuularegular;font-size: 25.73pt;font-weight: 400;color: #3b3b3b;">
               we cordially invite you to attend the ghH
           </td>
+      </tr> 
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 33.97pt;color: #3b3b3b;">
+            Date: Monday, 15 May 2017   
+        </td>
+      </tr> 
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 39.36pt;color: #3b3b3b;">
+            <b style="font-size: 52.23pt;">INAUGURATION</b>
+            <br>
+            of the Hilton Garden Inn, Ras Al Khaimah          
+        </td>
+      </tr>   
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 32.68pt;font-weight: 400;color: #3b3b3b;">
+            11am sharp – <b>Arrival</b>  <br>
+11:30am – <b>Ribbon cutting </b>  <br>
+11:45am – <b>Tours of the property</b>  <br>
+12:30pm - <b>Lunch </b>      
+        </td>
       </tr>
+
+            <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 28.05pt;color: #3b3b3b;">
+            Venue: Hilton Garden Inn,   <br>
+Bin Daher Street, Ras Al Khaimah          
+        </td>
+      </tr>
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 30.32pt;color: #3b3b3b;">
+            <u>Click here</u> for the location map.      
+        </td>
+      </tr>         
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 26.27pt;color: #3b3b3b;">
+            RSVP: Click here to confirm   <br>
+attendance or contact Carol Highmoor    
+        </td>
+      </tr>
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 26.27pt;color: #3b3b3b;">
+           on Email: carol@rakhospitality.com <br>
+ Phone: +971 7 2036021
+        </td>
+      </tr>
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 19.12pt;color: #3b3b3b;">
+          *This invite is non-transferable.
+        </td>
+      </tr>   
     </table>
 
     </body>
