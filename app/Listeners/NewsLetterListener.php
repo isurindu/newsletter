74dafa4bbbd7e8afd96884ce69cd33c0e7fb3b28
isurindu <isurindu.prabashwara@gmail.com>
2017-05-07 22:01:37 +0400

    first init

---
+++ app/Listeners/NewsLetterListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewsLetterEvent;
+use App\Mail\NewsLetterMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class NewsLetterListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  NewsLetterEvent  $event
+     * @return void
+     */
+    public function handle(NewsLetterEvent $event)
+    {
+        Mail::to('prabash.p@dubaimn.com')
+            ->cc('rifath.a@dubaimn.com')
+            ->send(new NewsLetterMail);
+    }
+}
---
+++ resources/views/mail/newsletter.blade.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+
+
+  <!--[if !mso]><!-->
+<link href="https://terceleye.net/hilton/fonts/stylesheet.css" rel="stylesheet" />
+
+{{--  <style>
+            h1 {
+                font-size: 24px;
+                color: #000;
+            }
+ @media screen {
+    @font-face {
+        font-family: 'capsuularegular';
+        src:local('capsuularegular'),local('capsuularegular'),local('capsuularegular'), url(http://dubaimn.net/hilton/fonts/capsuula-webfont.woff) format('woff'),
+            url(http://dubaimn.net/hilton/fonts/Capsuula.ttf) format('truetype'),
+            url(http://dubaimn.net/hilton/fonts/capsuula-webfont.woff2) format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+}
+        </style> --}}
+
+<!--<![endif]-->
+    </head>
+    <body style="margin:0;padding: 0;">
+
+         
+        <!--[if mso]>
+<style type="text/css">
+body, table, td {font-family: 'capsuularegular' !important;}
+</style>
+<![endif]-->
+<b style="font-family: 'capsuularegular';">Hgh we cordially invite you to attend the</b>
+    <table align="center" width="800" height="1400" background="https://terceleye.net/hilton/ver2-back.png">
+      <tr valign="top" height="279"><td height="279">&nbsp;</td></tr>
+      <tr valign="top" height="150" align="center">
+          <td style="font-family:'capsuularegular';font-size: 18.09pt;">
+            Under the patronage and presence of <br>
+            <b style="font-size: 21.2pt;">His Highness Sheikh Ahmed bin Saud bin Saqr Al Qasimi</b><br>
+            Chairman of RAK Hospitality Holding L.L.C
+          </td>
+      </tr>
+    
+      <tr valign="top" align="center">
+          <td  style="font-family:capsuularegular;font-size: 25.73pt;font-weight: 400;color: #3b3b3b;">
+              we cordially invite you to attend the ghH
+          </td>
+      </tr>
+    </table>
+
+    </body>
+</html>
